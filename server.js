eb32a154d84674f3e308f4dff1da4c3d8aaec42f
nandorfn <firnandorfn@gmail.com>
2023-04-25 20:33:56 +0700

    add port 3000

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const dbKey = process.env.MONGODB
 
 mongoose.connect(`mongodb+srv://${dbKey}`, {useNewUrlParser: true});
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
